a1b9de163017e039ff55149abef1799a6a07acc4
said Lansarov <saidlansarov12@gmail.com>
2022-12-21 17:14:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import AuthContextProvider from "./contexts/authContext";
+import ProductsContextProvider from "./contexts/productContext";
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <ProductsContextProvider>
+      <Navbar/>
+       <MainRoutes/>
+       </ProductsContextProvider>
+    </AuthContextProvider>
+     
+
   );
 }
 
